45662af317fe351fb90fca84b54612091119c177
Christina Chapman <cmchapman75@gmail.com>
2020-03-17 14:15:15 -0500

    Working on server issues

--- postgrator-config.js
+++ postgrator-config.js
@@ -6,7 +6,10 @@ module.exports = {
   'connectionString': (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL,
-  'ssl': !!process.env.SSL,
-  
-
+  'host': process.env.MIGRATION_DB_HOST,
+  'port': process.env.MIGRATION_DB_PORT,
+  'database': process.env.MIGRATION_DB_NAME,
+  'username': process.env.MIGRATION_DB_USER,
+  'password': process.env.MIGRATION_DB_PASS,
+  'ssl': !!process.env.SSL
 };
